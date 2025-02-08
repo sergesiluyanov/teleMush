75b081b7208dd2c7011be7c3a81dff4d5d062121
sergey <sergey@kinescope.io>
2025-02-08 23:29:06 +0400

    add model.py and train.py scripts

--- train.py
+++ train.py
@@ -1,3 +1,5 @@
+# model.py
+import tensorflow as tf
 from tensorflow.keras import layers, models
 
 
@@ -22,4 +24,12 @@ def create_model(input_shape=(224, 224, 3)):
                   loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'])
 
+    return model
+
+
+# Функция для обучения модели
+def train_model(X_train, y_train, X_val, y_val):
+    model = create_model()  # Создаем модель
+    model.fit(X_train, y_train, epochs=10, validation_data=(X_val, y_val))  # Обучаем модель
+    model.save('mushroom_model.h5')  # Сохраняем обученную модель
     return model
