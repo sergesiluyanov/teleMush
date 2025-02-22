78ef590034d2cbb300853ddd72e127f8669932b5
sergey <sergey@kinescope.io>
2025-02-22 08:46:00 +0400

    add custom dataset

--- predict.py
+++ predict.py
@@ -5,24 +5,24 @@ import numpy as np
 # Загружаем обученную модель
 model = load_model("mobilenetv2_mushroom.h5")
 
-# Словарь классов
-class_names = ['съедобный', 'несъедобный']
+# Список классов
+class_names = ['съедобный', 'несъедобный', 'другие_классы_грибов...']  # Замени на свои классы
 
 
 def predict_mushroom(image_path):
     # Загружаем изображение
     img = image.load_img(image_path, target_size=(224, 224))
     img_array = image.img_to_array(img) / 255.0  # Нормализация
-    img_array = np.expand_dims(img_array, axis=0)  # Добавляем batch dimension
+    img_array = np.expand_dims(img_array, axis=0)  # Добавляем batch размерность
 
     # Делаем предсказание
     prediction = model.predict(img_array)
-    class_idx = np.argmax(prediction)
+    class_idx = np.argmax(prediction)  # Индекс наиболее вероятного класса
 
     return class_names[class_idx]
 
 
 # Пример использования
-image_path = "test_mushroom.jpg"  # Укажи путь к изображению гриба
+image_path = "(Gemeine_Steinpilz)_Boletus_edulis.jpg"  # Замени на путь к изображению гриба
 result = predict_mushroom(image_path)
-print(f"Этот гриб: {result}")
+print(f"🔍 Этот гриб: {result}")
